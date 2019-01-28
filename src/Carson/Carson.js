ac0d9e674ac32dbe735fdaaca2327381c94bec0e
Carson Otto <carsonhockey15@gmail.com>
2019-01-28 15:57:02 -0600

    console logs hit blue on click of blue

--- src/Carson/Carson.js
+++ src/Carson/Carson.js
@@ -3,25 +3,25 @@ import '../App.css';
 const map = require('./map.jpg')
 
 class Carson extends Component {
-    
-  render() {
-    let screenLog = document.querySelector('#screen-log');
-    document.addEventListener('click', logKey);
-    
-    function logKey(e) {
-      console.log(`
-        Screen X/Y: ${e.screenX}, ${e.screenY}
-        Client X/Y: ${e.clientX}, ${e.clientY}`);
+
+    render() {
+        let screenLog = document.querySelector('#screen-log');
+        document.addEventListener('click', logKey);
+
+        function logKey(e) {
+            if (e.screenX > 440 && e.screenX < 510 && e.screenY > 200 && e.screenY < 270) {
+                console.log(`Hit blue`);
+            }
+        }
+
+        return (
+            <div>
+                <img src={map} />
+                <p>Your coordinates are:</p>
+                <p id="screen-log"></p>
+            </div>
+        );
     }
-    
-    return (
-      <div>
-          <img src={map} />
-          <p>Your coordinates are:</p>
-          <p id="screen-log"></p>
-      </div>
-    );
-  }
 }
 
 export default Carson;
