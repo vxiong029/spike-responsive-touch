0cd0944af6207559249d7d29020825662a041cf2
Carson Otto <carsonhockey15@gmail.com>
2019-01-28 16:17:24 -0600

    console logs which color you clicked on

--- src/Carson/Carson.js
+++ src/Carson/Carson.js
@@ -10,7 +10,13 @@ class Carson extends Component {
 
         function logKey(e) {
             if (e.screenX > 440 && e.screenX < 510 && e.screenY > 200 && e.screenY < 270) {
-                console.log(`Hit blue`);
+                console.log(`Hit Blue`);
+            } else if(e.screenX > 485 && e.screenX < 555 && e.screenY > 335 && e.screenY < 400){
+                console.log('Hit Green');
+            } else if(e.screenX > 345 && e.screenX < 410 && e.screenY > 295 && e.screenY < 365){
+                console.log('Hit Red');
+            } else{
+                return null;
             }
         }
 
