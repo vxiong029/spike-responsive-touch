fb4a586441b13a5b4934d9bdd2ff454e71489502
Vang Xiong <vxiong029@gmail.com>
2019-01-28 21:05:20 -0600

    animation spike

--- src/CircleOne.js
+++ src/CircleOne.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import CircleTwo from './CircleTwo';
 import CircleThree from './CircleThree';
 
@@ -10,13 +10,33 @@ const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerW
 
 class CircleOne extends Component {
   state = {
-    color: 'blue'
+    color: 'blue',
+    stroke: 'none'
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.color === 'yellow' && this.state.stroke === 'black') {
+      // when the state is updated (turned yellow), 
+      // a timeout is triggered to switch it back off
+      this.turnOffYellowTimeout = setTimeout(() => {
+        this.setState(() => ({ 
+          color: 'blue',
+          stroke: 'none'
+        }))
+      }, 500);
+    }
+  }
+  componentWillUnmount() {
+    // we set the timeout to this.turnOffYellowTimeout so that we can
+    // clean it up when the component is unmounted.
+    clearTimeout(this.turnOffYellowTimeout);
   }
   clickCircleOne = () => {
     console.log('in circle one');
+
     this.setState({
-      color: 'yellow'
-    })
+      color: 'yellow',
+      stroke: 'black'
+    });
   }
   render() {
     return (
@@ -29,9 +49,17 @@ class CircleOne extends Component {
           cx={10}
           cy={-500}
           r={50}
-          style={{ fill: this.state.color }}
+          style={{ fill: this.state.color, stroke: this.state.stroke }}
           onClick={this.clickCircleOne}
         />
+        <text 
+          textAnchor="middle" 
+          x="10" 
+          y="-500"
+          onClick={this.clickCircleOne}
+        >
+          Circle One
+        </text>
         <CircleTwo />
         <CircleThree />
       </svg>
--- src/CircleThree.js
+++ src/CircleThree.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 // const style = {
@@ -7,23 +7,53 @@ import './App.css';
 
 class CircleThree extends Component {
   state = {
-    color: 'green'
+    color: 'green',
+    stroke: 'none'
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.color === 'orange' && this.state.stroke === 'black') {
+      // when the state is updated (turned orange), 
+      // a timeout is triggered to switch it back off
+      this.turnOffOrangeTimeout = setTimeout(() => {
+        this.setState(() => ({ 
+          color: 'green',
+          stroke: 'none' 
+        }))
+      }, 500);
+    }
+  }
+  componentWillUnmount() {
+    // we set the timeout to this.turnOffOrangeTimeout so that we can
+    // clean it up when the component is unmounted.
+    clearTimeout(this.turnOffOrangeTimeout);
   }
   clickCircleThree = () => {
     console.log('in circle three');
+
     this.setState({
-      color: 'orange'
+      color: 'orange',
+      stroke: 'black'
     })
   }
   render() {
     return (
-      <circle
-        cx={-150}
-        cy={-300}
-        r={50}
-        style={{ fill: this.state.color }}
-        onClick={this.clickCircleThree}
-      />
+      <>
+        <circle
+          cx={-150}
+          cy={-300}
+          r={50}
+          style={{ fill: this.state.color, stroke: this.state.stroke }}
+          onClick={this.clickCircleThree}
+        />
+        <text 
+          textAnchor="middle" 
+          x="-150" 
+          y="-300"
+          onClick={this.clickCircleThree}
+        >
+          Circle Three
+        </text>
+      </>
     );
   }
 };
